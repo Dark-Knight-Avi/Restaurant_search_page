1b693d4da86b2fdebd3fe77fed2454e7b01cd746
Avi Karmakar <avimakhonkarmakar@gmail.com>
2021-09-11 13:16:55 +0530

    Restaurant

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function activeHover1() {
+    document.getElementById('navList1').style.cssText = 'color: #f1f1f1;';
+    document.getElementById('navList2').style.cssText = 'color: rgb(201, 199, 199);';
+    document.getElementById('navList3').style.cssText = 'color: rgb(201, 199, 199);';
+}
+
+function activeHover2() {
+    document.getElementById('navList2').style.cssText = 'color: #f1f1f1;';
+    document.getElementById('navList1').style.cssText = 'color: rgb(201, 199, 199);';
+    document.getElementById('navList3').style.cssText = 'color: rgb(201, 199, 199);';
+}
+
+function activeHover3() {
+    document.getElementById('navList3').style.cssText = 'color: #f1f1f1;';
+    document.getElementById('navList2').style.cssText = 'color: rgb(201, 199, 199);';
+    document.getElementById('navList1').style.cssText = 'color: rgb(201, 199, 199);';
+}
